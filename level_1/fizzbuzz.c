b01e2d0702061bb06898606e5687a7bc7a6e49d8
o.robert1994@hotmail.com <rokuni@f0r1s8.codam.nl>
2026-04-09 15:32:07 +0200

    news

--- level_1/fizzbuzz.c
+++ level_1/fizzbuzz.c
@@ -40,3 +40,49 @@ buzz
 $>
 
 */
+
+#include <unistd.h>
+
+void	ft_putnbr(int n)
+{
+	long nb;
+	char c;
+
+	nb = n;
+
+	// Handle negative numbers correctly
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = -nb;
+	}
+
+	// Print all digits except the last
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+
+	// Print last digit
+	c = (nb % 10) + '0';
+	write(1, &c, 1);
+}
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (n % 5 == 0)
+			write(1, "buzz", 4);
+		else if (n % 3 == 0)
+			write(1, "fizz", 4);
+		else
+			ft_putnbr(n);
+
+		write(1, "\n", 1);
+		n++;
+	}
+	return (0);
+}
