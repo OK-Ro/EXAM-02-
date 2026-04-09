81115c839171e81936092854c08b2292082a4b7b
o.robert1994@hotmail.com <rokuni@f0r1s8.codam.nl>
2026-04-09 12:57:03 +0200

    new

--- level_1/first_word.c
+++ level_1/first_word.c
@@ -35,19 +35,17 @@ int main(int argc, char **argv)
 {
 	int i = 0;
 
-	i = 0;
-
 	if (argc != 2)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-	if (argv[1][i] == ' ' || argv[1][i] == '\t')
+	while (argv[1][i] == ' ' || argv[1][i] == '\t')
 		i++;
-	if (!argv[1][i] == ' ' || !argv[1][i] == '\t')
+	while (argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
 	{
-		write(1, argv[1][i], 1);
-		argv[1][i]++;
+		write(1, &argv[1][i], 1);
+		i++;
 	}
 	write(1, "\n", 1);
 	return (0);
