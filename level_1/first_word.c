c8525228cdf2809fd432782b5fcfab178b5c2cd3
o.robert1994@hotmail.com <rokuni@f0r1s8.codam.nl>
2026-04-09 14:49:32 +0200

    done

--- level_1/first_word.c
+++ level_1/first_word.c
@@ -35,22 +35,18 @@ $>
 int main(int argc, char **argv)
 {
 	int i = 0;
-
-	if (argc != 2)
+	char *str;
+	if (argc !=2)
 	{
 		write(1, "\n", 1);
 		return (0);
 	}
-
-	char *str = argv[1];
-	char *str2 = argv[2];
+	str = argv[1];
 	while (str[i] == ' ' || str[i] == '\t')
 		i++;
-	while (str[i] = '\0' && str[i] != ' ' && str[i] != '\t')
+	while (str[i] != '\0' && str[i] !=  ' ' && str[i] != '\t')
 	{
 		write(1, &str[i], 1);
 		i++;
 	}
-	write(1, "\n", 1);
-	return (0);
 }
