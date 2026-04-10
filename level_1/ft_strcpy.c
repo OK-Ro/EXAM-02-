fb8f2a6f10a58557431a3ee37f623222a03fb0f2
o.robert1994@hotmail.com <rokuni@f1r4s19.codam.nl>
2026-04-10 16:07:11 +0200

    commit

--- level_1/ft_strcpy.c
+++ level_1/ft_strcpy.c
@@ -12,3 +12,19 @@ Your function must be declared as follows:
 char    *ft_strcpy(char *s1, char *s2);
 
 */
+#include <unistd.h>
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+	int i;
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	while (s2[i])
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
