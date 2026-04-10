cc7a70aa57f874f006b347af579d79f942cc0e20
o.robert1994@hotmail.com <rokuni@f1r4s19.codam.nl>
2026-04-10 16:34:25 +0200

    news

--- level_1/ulstr.c
+++ level_1/ulstr.c
@@ -36,13 +36,17 @@ int main(int argc, char **argv)
 	{
 		return(write(1, "\n", 1), 0);
 	}
-	str = argv;
-
+	str = argv[1];
+	i = 0;
 	while (str[i])
 	{
-		write(1, str[i], 1);
+		if ((str[i] >= 'a' && str[i] <= 'z'))
+			str[i] -= 32;
+		else if((str[i] >= 'A' && str[i] <= 'Z'))
+			str[i] += 32;
 		i++;
 	}
+	write(1, str, i);
 	write(1, "\n", 1);
 	return (0);
 }
