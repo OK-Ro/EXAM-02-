1989ccc447ec04130b695090a5d37980f49728a3
o.robert1994@hotmail.com <rokuni@f1r7s8.codam.nl>
2026-04-19 16:19:39 +0200

    commited

--- level_1/rotone.c
+++ level_1/rotone.c
@@ -30,3 +30,28 @@ $
 $>
 
 */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	char *str;
+
+	if(argc != 2)
+		return (write(1, "\n", 1),0);
+	str = argv[1];
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'y') || (str[i] >= 'A' && str[i] <= 'Y'))
+			str[i] = (str[i] + 1);
+		else if (str[i] == 'z')
+			str[i] = 'a';
+		else if (str[i] == 'Z')
+			str[i] = 'A';
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	return 0;
+}
--- level_1/search_and_replace.c
+++ level_1/search_and_replace.c
@@ -29,3 +29,26 @@ $>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
 eNcOre Un ExEmPle Pas Facile a Ecrire $
 
 */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i = 0;
+	char *str;
+
+	if (argc != 4)
+		return (write(1, "\n", 1), 0);
+	if (argv[2][1] != '\0' || argv[3][1] != '\0')
+		return (write(1, "\n", 1), 0);
+	str = argv[1];
+	while (str[i])
+	{
+		if (str[i] == argv[2][0])
+			write(1, &argv[3][0], 1);
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
